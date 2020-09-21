45f02a0589b11fe2abd3c59b1320e3ffb8b24fe8
Bob McCone <brudderbob07@gmail.com>
2020-09-22 10:58:49 +1200

    Updated authenticate.php
    Updated the authenticate.php file, as the person wants to be remembered and will keep them logged in for 7 days

--- CharlottesBlogWebsite/authenticate.php
+++ CharlottesBlogWebsite/authenticate.php
@@ -1,17 +1,5 @@
 <?php
-    session_start();
-
-    $DB_host = 'localhost';
-    $DB_user = 'root';
-    $DB_pass = 'mysql';
-    $DB_name = 'charlottesblog';
-
-    // Try and connect
-    $con = mysqli_connect($DB_host, $DB_user, $DB_pass, $DB_name);
-    if (mysqli_connect_errno()) {
-        // If connection failed, stop script and display error
-        exit('Failed to connect to MySQL: ' . mysqli_connect_error());
-    }
+    include 'main.php';
 
     // Now we check if the data from the login form was submitted, isset() will check if the data exists.
     if (!isset($_POST['username'], $_POST['password'])) {
@@ -20,34 +8,56 @@
     }
 
     // Prepare our SQL, preparing the SQL statement will prevent SQL injection.
-    if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
-        // Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
-        $stmt->bind_param('s', $_POST['username']);
-        $stmt->execute();
-        // Store the result so we can check if the account exists in the database.
-        $stmt->store_result();
+    $stmt = $con->prepare('SELECT id, password, remember_me, activation_code, role FROM accounts WHERE username = ?');
+    // Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
+    $stmt->bind_param('s', $_POST['username']);
+    $stmt->execute();
+    // Store the result so we can check if the account exists in the database.
+    $stmt->store_result();
 
-        if ($stmt->num_rows > 0) {
-            $stmt->bind_result($id, $password);
-            $stmt->fetch();
-            // Account exists, verifying the password
-            if (password_verify($_POST['password'], $password)) {
-            // If we don't want to use any password encryption method, we use this
-            // if ($_POST['password'] === $password) {
+    // Check if the account exists
+    if ($stmt->num_rows > 0) {
+        $stmt->bind_result($id, $password, $remember_me, $activation_code, $role);
+        $stmt->fetch();
+        $stmt->close();
+
+        // Account exists, verifying the password
+        // Note: remember to use password_hash in the registration file to store the hashed passwords
+        if (password_verify($_POST['password'], $password)) {
+        // If we don't want to use any password encryption method, we use this
+        // if ($_POST['password'] === $password) {
+            // Check if the account is activated
+            if (account_activation && $activation_code != 'activated') {
+                // User has not activated their account, output the message
+                echo 'Please activate your account to login, click <a href="resendactivation.php">here</a> to resend the activation email';
+            } else {
                 // Verification successful, user has logged in
                 // Create sessions so we know the user is logged in, they basically act like cookies but remember the data on the server
                 session_regenerate_id();
                 $_SESSION['loggedin'] = TRUE;
                 $_SESSION['name'] = $_POST['username'];
                 $_SESSION['id'] = $id;
-                header('Location: profile.php');
-            } else {
-                echo 'Incorrect username and/or password combination';
+                $_SESSION['role'] = $role;
+
+                // If the user checked the remember me check box
+                if (isset($_POST['remember_me'])) {
+                    // Create a hash that will be stored as a cookie and in the database, this will be used to identify the user
+                    $cookiehash = !empty($remember_me) ? $remember_me : password_hash($id . $_POST['username'] . 'yoursecretkey', PASSWORD_DEFAULT);
+                    // The amount of days a user will be remembered
+                    $days = 7;
+                    setcookie('remember_me', $cookiehash, (int)(time()+60*60*24*$days));
+                    // Update the remember_me field in the accounts table
+                    $stmt = $con->prepare('UPDATE accounts SET remember_me = ? WHERE id = ?');
+                    $stmt->bind_param('si', $cookiehash, $id);
+                    $stmt->execute();
+                    $stmt->close();
+                }
+                echo 'Success'; // Do not change this line as it will be used to check with the AJAX code
             }
         } else {
             echo 'Incorrect username and/or password combination';
         }
-
-        $stmt->close();
+    } else {
+        echo 'Incorrect username and/or password combination';
     }
 ?>
